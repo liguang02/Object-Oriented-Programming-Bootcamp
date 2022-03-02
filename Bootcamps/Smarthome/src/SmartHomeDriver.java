3cad6823795febcf1a70cfdda1c0cf17d76ef77f
lcha0068 <lcha0068@student.monash.edu>
2022-03-02 09:54:56 +0800

    committing and pushing 2 files(smartbulb and smarthomedriver) that has been modified to monash gitlab server

---
+++ Bootcamps/Smarthome/src/SmartHomeDriver.java
@@ -0,0 +1,36 @@
+public class SmartHomeDriver {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Smart Home System");
+        SmartBulb SB1 = new SmartBulb();
+        SmartBulb SB2 = new SmartBulb(true, "BLUE");
+        String c1 = SB1.getColour();
+        String c2 = SB2.getColour();
+        System.out.println(c1);
+        System.out.println(c2);
+
+        SB1.setColour("BLACK");
+        SB2.setColour("RED");
+        String c3 = SB1.getColour();
+        String c4 = SB2.getColour();
+        System.out.println(c3);
+        System.out.println(c4);
+
+        boolean s1 = SB1.getStatus();
+        boolean s2 = SB2.getStatus();
+        System.out.println(s1);
+        System.out.println(s2);
+
+        SB1.turnOn();
+        SB2.turnOff();
+
+//        boolean s3 = SB1.getStatus();
+//        boolean s4 = SB2.getStatus();
+//        System.out.println(s3);
+//        System.out.println(s4);
+        System.out.println(SB1.getConsumptionToday());
+
+    }
+
+}
