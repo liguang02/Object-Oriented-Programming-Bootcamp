5b580468e0a9a2117b731a9ed7460a6b33b20824
lcha0068 <lcha0068@student.monash.edu>
2022-03-09 02:42:41 +0800

    completed the tuesday lessons by coding along the lecture videos (inheritance).

---
+++ Bootcamps/Bootcamp02/Week2 - L02 - 2 - Inheritance_initialCode/src/SmartLighting.java
@@ -0,0 +1,30 @@
+public class SmartLighting extends SmartDevice {
+
+    private String colour;
+    private String type;
+    private int dimmingLevel;
+
+    //constructor
+    public SmartLighting(String _ipAddress, boolean _status, String _colour, String _type) {
+        super(_ipAddress, _status);
+        this.colour = _colour;
+        this.type=_type; // "dimmable or notDimmable"
+        this.dimmingLevel=100;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+
+    public void setDimmingLevel(int dimmingLevel) {
+        if (this.type=="dimmable")
+            this.dimmingLevel = dimmingLevel;
+    }
+
+
+}
---
+++ Bootcamps/Bootcamp02/Week2 - L02 - 2 - Inheritance_initialCode/src/SmartTV.java
@@ -0,0 +1,52 @@
+public class SmartTV extends SmartDevice{
+
+    //private attributes
+
+    private boolean isTimerOn;
+    private int timerMinutes;
+
+    //constructor
+    public SmartTV(String _ipAddress, boolean _status) {
+        super(_ipAddress,_status);
+
+    }
+
+    //getters and setters
+
+
+
+    //other methods
+
+    public void turnOn() {
+        super.turnOn();
+        System.out.println("The SmartTV at "+ super.getIpAddress() +" was turned ON succesfully");
+    }
+
+    public void turnOff() {
+        super.turnOff();
+        System.out.println("The SmartTV at "+ super.getIpAddress() +" was turned OFF succesfully");
+    }
+
+    public void setTimer(int _minutes)
+    {
+        this.isTimerOn=true;
+        this.timerMinutes=_minutes;
+
+        if(super.getStatus())
+        {
+
+            System.out.println("The smart TV at "+ super.getIpAddress() + " will turn on in " + this.timerMinutes + " minutes");
+            System.out.println("Call sent to the device.......");
+        }
+        else
+        {
+            System.out.println("The smart TV at "+ super.getIpAddress() + " will turn off in " + this.timerMinutes + " minutes");
+            System.out.println("Call sent to the device.......");
+        }
+
+    }
+
+    public void display() {
+        System.out.println("SmartTv  at " + getIpAddress() + " is on: " + getStatus() + " Today's consumption: " + super.getConsumptionToday());
+    }
+}
